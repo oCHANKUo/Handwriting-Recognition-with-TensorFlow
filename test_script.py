54c8013f6533c90ab7827a866197d5962957bcab
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-10-24 12:43:06 +0530

    changes

--- test_script.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import os
-
-MODEL_DIR = "saved_model_emnist_byclass"
-
-model = tf.keras.models.load_model(os.path.join(MODEL_DIR, 'best.keras'))
-
-model.export(MODEL_DIR)  
-print(f"Model saved to {MODEL_DIR}")
